dd3d2fb6d5da2ea2086abafb2e36c1d49ef7ca88
Justin McCarthy <uvatmvf@gmail.com>
2020-11-25 13:54:32 -0700

    added shopping cart service

--- ClientApp/src/app/product-details/product-details.component.ts
+++ ClientApp/src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../../cart.service';
 
 @Component({
     selector: 'app-product-details',
@@ -12,11 +13,17 @@ export class ProductDetailsComponent implements OnInit {
   product;
 
     /** product-details ctor */
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
     });
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product is added to the cart');
   }
 }
