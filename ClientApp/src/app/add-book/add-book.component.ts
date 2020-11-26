6d8afaa47bc65059642b6f3a76a38f998f1dfaed
Justin McCarthy <uvatmvf@gmail.com>
2020-11-25 17:10:07 -0700

    Adding the initial Add book component

---
+++ ClientApp/src/app/add-book/add-book.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-add-book',
+    templateUrl: './add-book.component.html',
+    styleUrls: ['./add-book.component.scss']
+})
+/** add-book component*/
+export class AddBookComponent {
+    /** add-book ctor */
+    constructor() {
+
+    }
+}
