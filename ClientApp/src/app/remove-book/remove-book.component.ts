b5e9e52dbfc2c19d0db694e880b66e012a28cd18
Justin McCarthy <uvatmvf@gmail.com>
2020-11-27 15:08:50 -0700

    add remove book component

---
+++ ClientApp/src/app/remove-book/remove-book.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-remove-book',
+    templateUrl: './remove-book.component.html',
+    styleUrls: ['./remove-book.component.scss']
+})
+/** remove-book component*/
+export class RemoveBookComponent {
+    /** remove-book ctor */
+    constructor() {
+
+    }
+}
