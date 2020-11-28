35ffeb971db97e879926f7d567c881961c01c738
Justin McCarthy <uvatmvf@gmail.com>
2020-11-28 14:14:57 -0700

    added imports and declarations to test spec

--- ClientApp/src/app/product-list/product-list.component.spec.ts
+++ ClientApp/src/app/product-list/product-list.component.spec.ts
@@ -1,7 +1,10 @@
-/// <reference path="../../../node_modules/@types/jasmine/index.d.ts" />
 import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
 import { BrowserModule, By } from "@angular/platform-browser";
 import { ProductListComponent } from './product-list.component';
+import { TopBarComponent } from '../top-bar/top-bar.component';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
+import { RouterModule } from '@angular/router';
+import { BooksComponent } from '../books/books.component';
 
 let component: ProductListComponent;
 let fixture: ComponentFixture<ProductListComponent>;
@@ -9,8 +12,17 @@ let fixture: ComponentFixture<ProductListComponent>;
 describe('product-list component', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ ProductListComponent ],
-            imports: [ BrowserModule ],
+          declarations: [ProductListComponent,
+            TopBarComponent,
+            ProductAlertsComponent
+          ],
+          imports: [BrowserModule,
+            RouterModule.forRoot([
+              { path: 'product-list', component: ProductListComponent },
+              { path: 'product-alerts', component: ProductAlertsComponent },
+              { path: 'top-bar', component: TopBarComponent }
+            ])
+          ],
             providers: [
                 { provide: ComponentFixtureAutoDetect, useValue: true }
             ]
